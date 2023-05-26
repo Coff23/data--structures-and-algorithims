7c2d0d8bf492f759701bb771cc2a05978676dd21
Joshua Coffey <itsjoshcoffey@gmail.com>
2023-05-26 16:32:31 -0500

    stacks

---
+++ javascript/code-challenges/stacks-and-queues/stacks.test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const { Stack } = require('./index');
+
+describe('Stack test', () => {
+  it('Push onto a stack', () => {
+    let stack = new Stack();
+
+    stack.push(1);
+
+    expect(stack.top.value).toEqual(1);
+    expect(stack.top.next).toBeNull();
+  });
+
+  it('Push multiple values on a stack', () => {
+    let stack = new Stack();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    expect(stack.top.value).toEqual(3);
+    expect(stack.top.next.value).toEqual(2);
+    expect(stack.top.next.next.value).toEqual(1);
+    expect(stack.top.next.next.next).toBeNull();
+  });
+
+  it('Pop off of the stack', () => {
+    let stack = new Stack();
+
+    stack.push(1);
+    stack.push(2);
+
+    expect(stack.top.value).toEqual(2);
+
+    stack.pop();
+
+    expect(stack.top.value).toEqual(1);
+  });
+});
